3f0c67e992d04091349920021d72bf3b05dcfee6
Guillaume Paigneau <paigneau.guillaume@gmail.com>
2024-01-29 11:29:57 +0100

    initial commit

---
+++ src/main/java/com/isika/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.isika;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping("/")
+	public String helloworld() {
+		return "{'message': 'Hello World V1'}";
+	}
+
+}
